b9cc0d4f09bfa464420c25218f8b485d670e9442
Kaffa-MY <meng-yang@foxmail.com>
2014-11-18 12:32:59 +0800

    设备页微调

--- src/com/familytouch/ui/DevicePage.java
+++ src/com/familytouch/ui/DevicePage.java
@@ -3,11 +3,15 @@ package com.familytouch.ui;
 import com.familytouch.R;
 import com.familytouch.R.id;
 import com.familytouch.R.layout;
+import com.familytouch.control.BackOnClickLlistener;
+import com.familytouch.control.HomeBtnOnClickListener;
+import com.familytouch.control.SearchBtnOnClickListener;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.CompoundButton;
+import android.widget.ImageButton;
 import android.widget.Switch;
 
 public class DevicePage extends Activity{
@@ -26,6 +30,16 @@ public class DevicePage extends Activity{
 				}
 			}
 		});
+		
+		ImageButton searchBtn = (ImageButton) findViewById(R.id.btn_search);
+		searchBtn.setOnClickListener(new SearchBtnOnClickListener(this));
+		ImageButton homeBtn = (ImageButton)findViewById(R.id.btn_home);
+		homeBtn.setOnClickListener(new HomeBtnOnClickListener(this));
+		ImageButton backBtn = (ImageButton)findViewById(R.id.btn_back);
+		backBtn.setOnClickListener(new BackOnClickLlistener(this));
+		
+		ImageButton iconBtn = (ImageButton)findViewById(R.id.btn_appicon);
+		iconBtn.setOnClickListener(new HomeBtnOnClickListener(this));
 	}
 
 }
